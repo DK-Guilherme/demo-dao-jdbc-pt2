430c45b8a8d97d834c4db0a52241ef01c3917539
Guilherme Dk <guilhermegsouza520@gmail.com>
2022-05-04 12:20:50 -0300

    FindAll implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,5 +24,11 @@ public class Main {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== TEST 3: seller findAll ====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
